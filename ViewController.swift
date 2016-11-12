d77d1658b901791771e908ebae9d40db0d364d30
noahhashmi <noahhashmi@gmail.com>
2016-11-11 23:18:12 -0500

    create project

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  posterpreter
+//
+//  Created by Noah Hashmi on 11/11/16.
+//  Copyright © 2016 Noah Hashmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
